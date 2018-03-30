784085a8e47b86373d4853efd1d99c1f0bdc2054
Andrew Cravenho <andrew@cravenho.me>
2018-03-30 14:15:44 -0400

    (Fix) Added error handling

--- app.js
+++ app.js
@@ -14,46 +14,60 @@ app.post('/api/verify', function (req, res) {
   var optimization = req.body.optimization
   var sourceCode = req.body.source
 
-  if (!address || !version || !name || !provider || !sourceCode) {
-    res.send('Missing variable')
-  }
-
   var web3 = new Web3(new Web3.providers.HttpProvider(provider))
   web3.eth.getCode(address).then(function (bytecode) {
     if (bytecode.substring(0, 2) === '0x') {
       bytecode = bytecode.substring(2)
-      bytecode = bytecode.substring(0, bytecode.length - 68)
+      var bytecodeCompare = bytecode.substring(0, bytecode.length - 68)
+      var swarm = bytecode.substring(bytecode.length - 68, bytecode.length - 4)
     }
 
-    if(!bytecode) {
-      res.send('Bytecode missing. Contract does not exist.')
-    }
     var data = {
+      'verified': '',
+      'error': '',
       'address': address,
       'compilerVersion': version,
       'optimization': optimization,
       'contractName': name,
-      'sourceCode': sourceCode
+      'sourceCode': sourceCode,
+      'swarm': swarm,
+      'bytecode': bytecode
       // 'sourceCode': 'pragma solidity 0.4.21;contract Test {int private count = 0;function getCount() public constant returns (int) {return count;}}'
     }
+
+    if (!address || !version || !name || !provider || !sourceCode) {
+      data.verified = 'no'
+      res.send(data)
+    }
+
+    if (!bytecode) {
+      data.verified = 'no'
+      res.send(data)
+    }
+
     try {
       if (version.substr(version.length - 8) === '(latest)') {
         var output = solc.compile(data.sourceCode, data.optimization)
-        ValidateCode(output, data, bytecode, res)
+        var send = ValidateCode(output, data, bytecodeCompare, res)
+        res.send(send)
       } else {
         solc.loadRemoteVersion(data.compilerVersion, function (err, solcV) {
           if (!err) {
             var output = solcV.compile(data.sourceCode, data.optimization)
-            var send = ValidateCode(output, data, bytecode, res)
+            var send = ValidateCode(output, data, bytecodeCompare, res)
             res.send(send)
           } else {
-            res.send(err)
+            data.verified = 'no'
+            data.error = err
+            res.send(data)
           }
         })
       }
       return
     } catch (e) {
-      res.send(e.stack)
+      data.verified = 'no'
+      data.error = e.stack
+      res.send(data)
     }
   })
 })
@@ -62,18 +76,19 @@ app.listen(PORT, () => console.log('Listening on port ' + PORT))
 
 var ValidateCode = function (output, data, bytecode, response) {
   if (!output.contracts || !output.contracts[data.contractName]) {
-    data.valid = false
+    data.verified = 'no'
+    data.error = 'Contract does not exist or name does not match'
     return data
   } else {
     var runtime = output.contracts[data.contractName].runtimeBytecode
     runtime = runtime.substring(0, runtime.length - 68)
     if (runtime.indexOf(bytecode) > -1) {
-      data.valid = true
+      data.verified = 'yes'
       data.abi = output.contracts[data.contractName].interface
-      data.byteCode = bytecode
       return data
     } else {
-      data.valid = false
+      data.verified = 'no'
+      data.error = 'Bytecode does not match'
       return data
     }
   }
